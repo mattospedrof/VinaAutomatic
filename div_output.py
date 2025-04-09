a8119d72ffd4199fc5541e39f47a6917b518a334
Pedro de Mattos Franco <francobiomed@gmail.com>
2025-04-08 21:51:57 -0400

    feat: extracting all models

--- div_output.py
+++ div_output.py
@@ -1,43 +1,39 @@
 import os
+import configparser
 
 
-def read_pdbqt(number_model, name_archive_pdbqt, name_desired_file):
+def read_pdbqt(name_archive_pdbqt, output_prefix):
     with open(name_archive_pdbqt, 'r') as origin_pdbqt:
         content_in_origin = origin_pdbqt.read()
-        number_model = model_number_desired
 
-        init_model = content_in_origin.find(f"MODEL {number_model}")
+    model_number = 1
+    while True:
+        init_model = content_in_origin.find(f"MODEL {model_number}")
         if init_model == -1:
-            print(f"The model entered '{number_model}' was not found.")
-            return
-
+            break
+        
         next_model = content_in_origin.find("MODEL ", init_model + 1)
-
-        if next_model == -1:
-            end_model = len(content_in_origin)
-        else:
-            end_model = next_model
-
+        end_model = next_model if next_model != -1 else len(content_in_origin)
         excerpt_model = content_in_origin[init_model:end_model].strip()
 
+        name_desired_file = f"{output_prefix}_{model_number}.pdbqt"
         with open(name_desired_file, 'w') as desired_file:
             desired_file.write(excerpt_model)
-
-        print(
-            f"\nThe model excerpt {number_model} was written in the file {name_desired_file}.\n"
-            "Thank you for your support\n"
-            "For any question, visit the github:\n"
-            "https://github.com/mattospedrof/Automatic-Autodock-Vina"
-        )
+        
+        print(f"The model excerpt {model_number} was written in the file {name_desired_file}.")
+        model_number += 1
 
 
-origin_pdbqt = input("Enter the file name pdbqt with the '.pdbqt': ")
+config = configparser.ConfigParser()
+config.read("config.txt")
+number_rounds = config.getint('Docking', 'num_rounds') + 1
 
-if not os.path.exists(origin_pdbqt):
-    print(f"The file '{origin_pdbqt}' does not exist.")
-else:
-    model_number_desired = input(
-        "Enter the model number you want to extract: ")
-    model_number_desired = int(model_number_desired)
-    desired_file = f"{os.path.splitext(origin_pdbqt)[0]}_{model_number_desired}.pdbqt"
-    read_pdbqt(model_number_desired, origin_pdbqt, desired_file)
+for n in range(1, number_rounds):
+    origin_pdbqt = f"output_docking_{n}.pdbqt"
+    if os.path.exists(origin_pdbqt):
+        output_prefix = f"output_docking_{n}"
+        read_pdbqt(origin_pdbqt, output_prefix)
+    else:
+        print(f"The file '{origin_pdbqt}' does not exist.")
+
+print("Files successfully divided by models.")
